5cc5f8c91533f41a4ee84a865dc00b72ca51f8f2
Cyril <cyriltiger@gmail.com>
2024-11-30 21:59:36 +0100

    remove iife in resolve

--- lib/resolve.ts
+++ lib/resolve.ts
@@ -1,11 +1,9 @@
-(async function () {
-  const [year, day] = Deno.args;
+const [year, day] = Deno.args;
 
-  const path = `${Deno.cwd()}/${year}/${day}`;
+const path = `${Deno.cwd()}/${year}/${day}`;
 
-  const input = await Deno.readTextFile(`${path}/input.txt`);
-  const { part01, part02 } = await import(`${path}/solutions.ts`);
+const input = await Deno.readTextFile(`${path}/input.txt`);
+const { part01, part02 } = await import(`${path}/solutions.ts`);
 
-  console.log("part 01", part01(input));
-  console.log("part 02", part02(input));
-})();
+console.log("part 01", part01(input));
+console.log("part 02", part02(input));
